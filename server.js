cce95134c3da2d7a22bb6da4e9868f5fd1cb20b1
dustinjack99 <57464275+dustinjack99@users.noreply.github.com>
2020-04-17 16:25:51 -0700

    more playing with deployment

--- server.js
+++ server.js
@@ -1,30 +1,30 @@
-//EXPRESS SERVER CODE
-const express = require("express");
-const path = require("path");
+// //EXPRESS SERVER CODE
+// const express = require("express");
+// const path = require("path");
 
-const app = express();
-const PORT = process.env.PORT || 8080;
+// const app = express();
+// const PORT = process.env.PORT || 8080;
 
-app.use(express.static("public"));
+// app.use(express.static("public"));
 
-//middleware launching - handles data parsing with Express
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+// //middleware launching - handles data parsing with Express
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 
-app.listen(PORT, function () {
-  // Log (server-side) when our server has started
-  console.log("Server listening on: http: //localhost:" + PORT);
-});
+// app.listen(PORT, function () {
+//   // Log (server-side) when our server has started
+//   console.log("Server listening on: http: //localhost:" + PORT);
+// });
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-app.get("/index", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/index.html"));
-});
-app.get("/portfolio", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/portfolio.html"));
-});
-app.get("/contact", (req, res) => {
-  res.sendFile(path.join(__dirname, "public/contact.html"));
-});
+// app.get("/", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+// app.get("/index", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/index.html"));
+// });
+// app.get("/portfolio", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/portfolio.html"));
+// });
+// app.get("/contact", (req, res) => {
+//   res.sendFile(path.join(__dirname, "public/contact.html"));
+// });
